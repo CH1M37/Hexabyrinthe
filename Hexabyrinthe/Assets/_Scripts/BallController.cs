7a93eb5b80255026efefe58119f8ddd7ff918474
CH1M37 <CH1M37>
2018-11-29 16:58:32 +0100

    1ers tests lumiere

---
+++ Hexabyrinthe/Assets/_Scripts/BallController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+    public float speed;
+
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal * speed, 0.0f, moveVertical * speed);
+
+        transform.Translate(movement);
+    }
+
+}
