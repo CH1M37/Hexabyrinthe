b59985deb56852f8aee1f8a80a805ba183b34329
grusscharles <grusscharles@gmail.com>
2018-11-29 17:15:34 +0100

    Mise en place editeur

---
+++ Hexabyrinthe/Assets/_Scripts/GridHex.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridHex : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject tilePrefab;
+
+    public int gridSize;
+    public float gridScale;
+
+    #region Grid Creation
+
+    public void ResetGrid()
+    {
+        GameObject[] tilesArray = GameObject.FindGameObjectsWithTag("ground");
+
+        foreach (GameObject tileToDestroy in tilesArray)
+        {
+            DestroyImmediate(tileToDestroy);
+        }
+
+    }
+
+    public void CreateGrid(int n, float scale)
+    {
+        for(int i=0; i<n;i++)
+        {
+            //CreateGrid
+            for(int j=0; j<n; j++)
+            {
+                GameObject tile = Instantiate(tilePrefab);
+                tile.transform.SetParent(this.transform, false);
+                tile.transform.position = Vector3.zero;
+                tile.transform.localScale = new Vector3(scale, tile.transform.localScale.y, scale);
+
+                tile.transform.position = new Vector3(scale * i, 0f, scale * j);
+
+                //Color
+                if ((i % 2 == 0 && j % 2 == 0) || (i % 2 == 1 && j % 2 == 1))
+                {
+                    Renderer rend = tile.GetComponent<Renderer>();
+                    rend.material.color = Color.black;
+                }
+            }
+        }
+    }
+    #endregion
+
+}
